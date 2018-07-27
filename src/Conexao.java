fbe87dd4693c07af880cead64584f9669c263c9a
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-07-27 01:32:41 -0300

    Adiciona outros protocolos

--- src/Conexao.java
+++ src/Conexao.java
@@ -34,18 +34,18 @@ public class Conexao implements Runnable {
   }
 
   private String getPath(String path) {
-    return path.equals("/") ? root + "/index.html" : root + path;
+    return path.equals("/") ? "./" + root + "/index.html" : "./" + root + path;
   }
 
   private void getInfoRequest(String request) {
+    if (request == null) return;
     String[] lines = request.split(System.lineSeparator());
+    String[] methodLine = lines[0].split(" ");
+    requestMethod = methodLine[0];
+    requestPath = methodLine[1];
+    requestHTTPVersion = methodLine[2];
+
     for (String line : lines) {
-      if (line.contains("GET")) {
-        String[] methodLine = line.split(" ");
-        requestMethod = methodLine[0];
-        requestPath = methodLine[1];
-        requestHTTPVersion = methodLine[2];
-      }
       if (line.contains("Host: ")) {
         String[] hostLine = line.split(" ")[1].split(":");
         requestHost = hostLine[0];
@@ -59,59 +59,61 @@ public class Conexao implements Runnable {
     // System.out.println("Port: " + requestPort);
   }
 
-  private void connectionHandler(String request) {
-    try {
-      // Envia para o cliente (browser)
-      OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
-      // Manipula cabeçalho para retirar informacoes importantes
-      this.getInfoRequest(request);
-      // Recupera caminho do arquivo solicitado
-      String requestedFileString = this.getPath(requestPath);
-      // Verificar se o arquivo existe
-      Path requestedFilePath = Paths.get(requestedFileString);
-      if (!Files.exists(requestedFilePath)) {
-        // Nao possui o arquivo solicitado, retorna erro 404
-        responseHeader = "HTTP/1.1 404 NOT FOUND\nContent-type: text/html\n\n";
-        responseContent = this.readFile(this.getPath("/notFound.html"));
-      } else {
-        // ok
-        responseHeader = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
-        responseContent = this.readFile(requestedFileString);
-      }
-      out.write(responseHeader);
-      out.write(responseContent);
-
-      out.flush();
-      out.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-      System.out.println(e.getMessage());
-
-    }
-  }
-
-  private String readFile(String path) {
+  private String readFile(String path) throws IOException {
     String result = "";
+    BufferedReader br = new BufferedReader(new FileReader(path));
+    StringBuilder sb = new StringBuilder();
+    String lineFile = null;
 
     try {
-      // Lendo arquivo index.html
-      BufferedReader br = new BufferedReader(new FileReader(path));
-      StringBuilder sb = new StringBuilder();
-      String lineFile = null;
-
       while ((lineFile = br.readLine()) != null) {
         sb.append(lineFile);
         sb.append(System.lineSeparator());
       }
 
       result = sb.toString();
+    } finally {
       br.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
     }
 
     return result;
+  }
+
+  private void connectionHandler(String request) throws IOException {
+    OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+    // Manipula cabeçalho para retirar informacoes importantes
+    this.getInfoRequest(request);
+
+    // Recupera caminho do arquivo solicitado
+    if (requestPath == null) return;
+
+    String requestedFileString = this.getPath(requestPath);
+    Path requestedFilePath = Paths.get(requestedFileString);
+
+    // Verificar o metodo solicitado
+    if (!requestMethod.equals("GET")) {
+      // Nao possui o metodo implementado (PUT, POST, DELETE), retorna erro 501
+      responseHeader = "HTTP/1.1 501 Not Implemented\nContent-type: text/html\n\n";
+      responseContent = "<h1>Erro 501 Not Implemented</h1>";
+    }
+    // Verificar se o arquivo existe
+    else if (!Files.exists(requestedFilePath)) {
+      // Nao possui o arquivo solicitado, retorna erro 404
+      responseHeader = "HTTP/1.1 404 Not Found\nContent-type: text/html\n\n";
+      responseContent = this.readFile(this.getPath("/notFound.html"));
+    } else {
+      // ok
+      responseHeader = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
+      responseContent = this.readFile(requestedFileString);
+    }
+
+    try {
+      out.write(responseHeader);
+      out.write(responseContent);
+    } finally {
+      out.flush();
+      out.close();
+    }
   }
 
   public void run() {
@@ -120,8 +122,8 @@ public class Conexao implements Runnable {
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "ASCII"));
 
       // Lendo requisicao (header + content)
-      String line = null;
-      String request = "";
+      String line = in.readLine();
+      String request = line;
       while ((line = in.readLine()) != null) {
         request = request + "\r\n" + line;
         if (line.length() == 0)
